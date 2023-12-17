357f5dc1acdcc813874e12cca23a4fe83f89313c
Dudkin Vladimir <vdudkin@metaprime.ru>
2023-12-17 14:27:47 +0300

    deploy

--- src/ui-kit/components/img/UiImg.vue
+++ src/ui-kit/components/img/UiImg.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="ui-img">
-    <img class="ui-img__img-content" :src="src" :alt="alt">
+    <img class="ui-img__img-content" :src="calcSrc" :alt="alt">
   </div>
 </template>
 
 <script setup>
-defineProps({
+import { computed } from 'vue';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const props = defineProps({
   src: {
     type: String,
     default: null,
@@ -16,6 +20,17 @@ defineProps({
     default: null,
     requred: true,
   },
+});
+
+const calcSrc = computed(() => {
+  if (!props.src) {
+    console.error('src is not found');
+    return null;
+  }
+
+  if (isProduction) return `/tages-test${props.src}`;
+
+  return props.src;
 });
 </script>
 
